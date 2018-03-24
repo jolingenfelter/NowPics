a28e8f5565d2eb4ca470b28319abbb9af478e8e5
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2018-03-24 13:16:46 -0400

    InstagramClient.plist entries

---
+++ NowPicsGallery/NowPicsGallery/InstagramClient.swift
@@ -0,0 +1,9 @@
+//
+//  InstagramClient.swift
+//  NowPicsGallery
+//
+//  Created by Joanna LINGENFELTER on 3/24/18.
+//  Copyright © 2018 Joanna LINGENFELTER. All rights reserved.
+//
+
+import Foundation
