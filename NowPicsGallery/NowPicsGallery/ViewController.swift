e2c1df96dae0431476f724156bb92ad71f0c45e1
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2018-03-24 12:59:00 -0400

    Add Xcode project

---
+++ NowPicsGallery/NowPicsGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NowPicsGallery
+//
+//  Created by Joanna LINGENFELTER on 3/24/18.
+//  Copyright © 2018 Joanna LINGENFELTER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
